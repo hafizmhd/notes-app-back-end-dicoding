f4038462e6112d8c541efb02291f7955a0a348ad
Ahmad Hafiz Mahardika <fizdaryza1@gmail.com>
2024-01-26 15:23:14 +0700

    modified:   src/handler.js adding 'untitled'

--- src/handler.js
+++ src/handler.js
@@ -2,7 +2,7 @@ const { nanoid } = require('nanoid');
 const notes = require('./notes');
 
 const addNoteHandler = (request, h) => {
-  const { title, tags, body } = request.payload;
+  const { title = 'untitled', tags, body } = request.payload;
   const id = nanoid(16);
   const createdAt = new Date().toISOString();
   const updatedAt = createdAt;
