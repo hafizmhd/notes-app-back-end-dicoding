12426f099d2d53120ef1a39deca2bc20040f9d55
Ahmad Hafiz Mahardika <fizdaryza1@gmail.com>
2024-02-01 05:59:53 +0700

    postgres database implemented

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -11,10 +11,10 @@ class NotesHandler {
     this.deleteNoteByIdHandler = this.deleteNoteByIdHandler.bind(this);
   }
 
-  postNoteHandler(request, h) {
+  async postNoteHandler(request, h) {
     this._validator.validateNotePayload(request.payload);
     const { title = 'untitled', tags, body } = request.payload;
-    const noteId = this._service.addNote({ title, tags, body });
+    const noteId = await this._service.addNote({ title, tags, body });
 
     const response = h.response({
       status: 'success',
@@ -27,8 +27,8 @@ class NotesHandler {
     return response;
   }
 
-  getNotesHandler() {
-    const notes = this._service.getNotes();
+  async getNotesHandler() {
+    const notes = await this._service.getNotes();
     return {
       status: 'success',
       data: {
@@ -37,9 +37,9 @@ class NotesHandler {
     };
   }
 
-  getNoteByIdHandler(request) {
+  async getNoteByIdHandler(request) {
     const { id } = request.params;
-    const note = this._service.getNoteById(id);
+    const note = await this._service.getNoteById(id);
 
     return {
       status: 'success',
@@ -49,10 +49,10 @@ class NotesHandler {
     };
   }
 
-  putNoteByIdHandler(request) {
+  async putNoteByIdHandler(request) {
     this._validator.validateNotePayload(request.payload);
     const { id } = request.params;
-    this._service.editNoteById(id, request.payload);
+    await this._service.editNoteById(id, request.payload);
 
     return {
       status: 'success',
@@ -60,10 +60,10 @@ class NotesHandler {
     };
   }
 
-  deleteNoteByIdHandler(request) {
+  async deleteNoteByIdHandler(request) {
     const { id } = request.params;
 
-    this._service.deleteNoteById(id);
+    await this._service.deleteNoteById(id);
     return {
       status: 'success',
       message: 'Catatan berhasil dihapus',
